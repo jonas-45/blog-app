dd61e10bde7168b7195763982d5397b7bb97c7e0
jonas-45 <jonaswullah@gmail.com>
2023-07-15 06:31:38 +0000

    Change user to author in model associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
-class Comments < ApplicationRecord
-  belongs_to :user, foreign_key: :author_id
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post, foreign_key: :post_id
 end

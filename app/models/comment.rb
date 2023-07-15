21fb81f3ccb51110964465a482523fb564ae5e13
jonas-45 <jonaswullah@gmail.com>
2023-07-15 08:37:33 +0000

    Add after save hook to update comments and likes counters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post, foreign_key: :post_id
 
+  after_save :update_comments_counter
+
+  private
+  
   def update_comments_counter
     post.update(comments_counter: post.comments_counter + 1)
   end

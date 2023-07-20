dcf932e3a91caf1f4c454b8a35214d94e0922789
jonas-45 <jonaswullah@gmail.com>
2023-07-20 21:58:11 +0000

    Retrieve user and recent post in the show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,8 @@ class UsersController < ApplicationController
     @all_users = User.all
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    @recent_post = @user.three_recent_posts
+  end
 end

0a1ad7b48e8041adbefa771015e068973e60c6be
jonas-45 <jonaswullah@gmail.com>
2023-07-20 22:08:44 +0000

    Install stylelint packages from npm

--- db/migrate/20230715065758_rename_post_counter.rb
+++ db/migrate/20230715065758_rename_post_counter.rb
@@ -1,4 +1,4 @@
-class RenamePostCounter < ActiveRecord::Migration[7.0]
+ class RenamePostCounter < ActiveRecord::Migration[7.0]
   def change
     rename_column :users, :postsCounter, :post_counter
   end

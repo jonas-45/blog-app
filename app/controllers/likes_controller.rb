31367dace1dc86953fcda2b02a7126b0bb20c87e
jonas-45 <jonaswullah@gmail.com>
2023-07-22 18:07:12 +0000

    Add new action to likes controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,15 @@
+class LikesController < ApplicationController
+  def new
+    @like = Like.new
+    @like.author = current_user
+    @like.post = Post.find(params[:post_id])
+    if @like.save
+      flash[:notice] = 'Like added successfully!'
+      redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id])
+    else
+      flash[:alert] = "Couldn't add a like!"
+      puts @like.errors.full_messages
+      render :new, status: :unprocessable_entity
+    end
+  end
+end

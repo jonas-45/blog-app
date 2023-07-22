1ebcf6a86ee85f478483d05aa49613d583d78781
jonas-45 <jonaswullah@gmail.com>
2023-07-22 18:06:49 +0000

    Add new and create actions to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,35 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @user_posts = Post.where(author_id: @user)
+    #@post_comments = Comment.where(post_id: @user_posts.ids)
     @post_comments = Comment.where(post_id: @user_posts.ids)
   end
 
   def show
     @post = Post.find(params[:id])
     @post_comments = Comment.where(post_id: @post)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+
+    if @post.save
+      flash[:notice] = 'Post created successfully!'
+      redirect_to user_posts_path(current_user)
+    else
+      flash[:alert] = "Couln't create post!"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end

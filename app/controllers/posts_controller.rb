df91e1b88bb1646f3b120098fbffd9a1e89634d8
jonas-45 <jonaswullah@gmail.com>
2023-07-29 18:03:36 +0000

    Add destroy and authorisation configuration to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,17 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+
   def index
     @user = User.find(params[:user_id])
     @user_posts = Post.includes(:comments).where(author_id: @user)
     @post_comments = Comment.includes(:author).where(post_id: @user_posts.ids)
+    authorize! :read, Post
   end
 
   def show
     @post = Post.includes(:comments).find(params[:id])
     @post_comments = Comment.includes(:author).where(post_id: @post)
+    @user = current_user
   end
 
   def new
@@ -24,6 +28,17 @@ class PostsController < ApplicationController
     else
       flash[:alert] = "Couln't create post!"
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    # @post.destroy
+    @post = Post.find(params[:id])
+    if @post&.destroy
+      flash[:success] = 'Post deleted!'
+      @post.author.decrement!(:posts_counter) # Decrease the post count by 1 for the post's author
+    else
+      flash[:danger] = 'Post not deleted!'
     end
   end
 
